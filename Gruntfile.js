198450fb82c79d57fe6fa0be25227a89c0b9f419
englishextra <englishextra@yandex.ru>
2017-05-04 15:30:41 +0300

    tidying up

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,9 @@
-module.exports = function (grunt) { 
-	grunt.initConfig({ 
-		jshint: { 
-			all: ["qrjs2.js"] 
-		} 
-	}); 
-	grunt.loadNpmTasks("grunt-contrib-jshint"); 
-	grunt.registerTask("default", "jshint"); 
-}; 
+module.exports = function (grunt) {
+	grunt.initConfig({
+		jshint: {
+			all: ["qrjs2.js"]
+		}
+	});
+	grunt.loadNpmTasks("grunt-contrib-jshint");
+	grunt.registerTask("default", "jshint");
+};
