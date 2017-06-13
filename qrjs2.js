43c27195997fe3a453e4c71647661647373ddcb6
englishextra <englishextra@yandex.ru>
2017-06-13 20:14:13 +0300

    updates

--- qrjs2.js
+++ qrjs2.js
@@ -17,7 +17,7 @@
 /* jshint -W041 */
 (function (root, name, definition) {
 	root[name] = definition();
-})("undefined" !== typeof window ? window : this, 'QRCode', function () {
+})("undefined" !== typeof window ? window : this, "QRCode", function () {
 	var VERSIONS = [null, [[10, 7, 17, 13], [1, 1, 1, 1], []], [[16, 10, 28, 22], [1, 1, 1, 1], [4, 16]], [[26, 15, 22, 18], [1, 1, 2, 2], [4, 20]], [[18, 20, 16, 26], [2, 1, 4, 2], [4, 24]], [[24, 26, 22, 18], [2, 1, 4, 4], [4, 28]], [[16, 18, 28, 24], [4, 2, 4, 4], [4, 32]], [[18, 20, 26, 18], [4, 2, 5, 6], [4, 20, 36]], [[22, 24, 26, 22], [4, 2, 6, 6], [4, 22, 40]], [[22, 30, 24, 20], [5, 2, 8, 8], [4, 24, 44]], [[26, 18, 28, 24], [5, 4, 8, 8], [4, 26, 48]], [[30, 20, 24, 28], [5, 4, 11, 8], [4, 28, 52]], [[22, 24, 28, 26], [8, 4, 11, 10], [4, 30, 56]], [[22, 26, 22, 24], [9, 4, 16, 12], [4, 32, 60]], [[24, 30, 24, 20], [9, 4, 16, 16], [4, 24, 44, 64]], [[24, 22, 24, 30], [10, 6, 18, 12], [4, 24, 46, 68]], [[28, 24, 30, 24], [10, 6, 16, 17], [4, 24, 48, 72]], [[28, 28, 28, 28], [11, 6, 19, 16], [4, 28, 52, 76]], [[26, 30, 28, 28], [13, 6, 21, 18], [4, 28, 54, 80]], [[26, 28, 26, 26], [14, 7, 25, 21], [4, 28, 56, 84]], [[26, 28, 28, 30], [16, 8, 25, 20], [4, 32, 60, 88]], [[26, 28, 30, 28], [17, 8, 25, 23], [4, 26, 48, 70, 92]], [[28, 28, 24, 30], [17, 9, 34, 23], [4, 24, 48, 72, 96]], [[28, 30, 30, 30], [18, 9, 30, 25], [4, 28, 52, 76, 100]], [[28, 30, 30, 30], [20, 10, 32, 27], [4, 26, 52, 78, 104]], [[28, 26, 30, 30], [21, 12, 35, 29], [4, 30, 56, 82, 108]], [[28, 28, 30, 28], [23, 12, 37, 34], [4, 28, 56, 84, 112]], [[28, 30, 30, 30], [25, 12, 40, 34], [4, 32, 60, 88, 116]], [[28, 30, 30, 30], [26, 13, 42, 35], [4, 24, 48, 72, 96, 120]], [[28, 30, 30, 30], [28, 14, 45, 38], [4, 28, 52, 76, 100, 124]], [[28, 30, 30, 30], [29, 15, 48, 40], [4, 24, 50, 76, 102, 128]], [[28, 30, 30, 30], [31, 16, 51, 43], [4, 28, 54, 80, 106, 132]], [[28, 30, 30, 30], [33, 17, 54, 45], [4, 32, 58, 84, 110, 136]], [[28, 30, 30, 30], [35, 18, 57, 48], [4, 28, 56, 84, 112, 140]], [[28, 30, 30, 30], [37, 19, 60, 51], [4, 32, 60, 88, 116, 144]], [[28, 30, 30, 30], [38, 19, 63, 53], [4, 28, 52, 76, 100, 124, 148]], [[28, 30, 30, 30], [40, 20, 66, 56], [4, 22, 48, 74, 100, 126, 152]], [[28, 30, 30, 30], [43, 21, 70, 59], [4, 26, 52, 78, 104, 130, 156]], [[28, 30, 30, 30], [45, 22, 74, 62], [4, 30, 56, 82, 108, 134, 160]], [[28, 30, 30, 30], [47, 24, 77, 65], [4, 24, 52, 80, 108, 136, 164]], [[28, 30, 30, 30], [49, 25, 81, 68], [4, 28, 56, 84, 112, 140, 168]]];
 	var MODE_TERMINATOR = 0;
 	var MODE_NUMERIC = 1,
@@ -51,7 +51,7 @@
 	}
 	var ALPHANUMERIC_MAP = {};
 	for (var i = 0; i < 45; ++i) {
-		ALPHANUMERIC_MAP['0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ $%*+-./:'.charAt(i)] = i;
+		ALPHANUMERIC_MAP["0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ $%*+-./:".charAt(i)] = i;
 	}
 	var MASKFUNCS = [function (i, j) {
 			return (i + j) % 2 == 0;
@@ -80,8 +80,9 @@
 	var nfullbits = function (ver) {
 		var v = VERSIONS[ver];
 		var nbits = 16 * ver * ver + 128 * ver + 64;
-		if (needsverinfo(ver))
+		if (needsverinfo(ver)) {
 			nbits -= 36;
+		}
 		if (v[2].length) {
 			nbits -= 25 * v[2].length * v[2].length - 10 * v[2].length - 55;
 		}
@@ -121,15 +122,17 @@
 	var validatedata = function (mode, data) {
 		switch (mode) {
 		case MODE_NUMERIC:
-			if (!data.match(NUMERIC_REGEXP))
+			if (!data.match(NUMERIC_REGEXP)) {
 				return null;
+			}
 			return data;
 		case MODE_ALPHANUMERIC:
-			if (!data.match(ALPHANUMERIC_REGEXP))
+			if (!data.match(ALPHANUMERIC_REGEXP)) {
 				return null;
+			}
 			return data.toUpperCase();
 		case MODE_OCTET:
-			if (typeof data === 'string') {
+			if (typeof data === "string") {
 				var newdata = [];
 				for (var i = 0; i < data.length; ++i) {
 					var ch = data.charCodeAt(i);
@@ -162,8 +165,9 @@
 				bits = 0;
 				remaining = 8;
 			}
-			if (n > 0)
+			if (n > 0) {
 				bits |= (x & ((1 << n) - 1)) << (remaining -= n);
+			}
 		};
 		var nlenbits = ndatalenbits(ver, mode);
 		pack(mode, 4);
@@ -191,12 +195,15 @@
 			break;
 		}
 		pack(MODE_TERMINATOR, 4);
-		if (remaining < 8)
+		if (remaining < 8) {
 			buf.push(bits);
-		while (buf.length + 1 < maxbuflen)
+		}
+		while (buf.length + 1 < maxbuflen) {
 			buf.push(0xec, 0x11);
-		if (buf.length < maxbuflen)
+		}
+		if (buf.length < maxbuflen) {
 			buf.push(0xec);
+		}
 		return buf;
 	};
 	var calculateecc = function (poly, genpoly) {
@@ -253,8 +260,9 @@
 	var augumentbch = function (poly, p, genpoly, q) {
 		var modulus = poly << q;
 		for (var i = p - 1; i >= 0; --i) {
-			if ((modulus >> (q + i)) & 1)
+			if ((modulus >> (q + i)) & 1) {
 				modulus ^= genpoly << i;
+			}
 		}
 		return (poly << q) | modulus;
 	};
@@ -311,8 +319,9 @@
 		var k = 0,
 		dir = -1;
 		for (var i = n - 1; i >= 0; i -= 2) {
-			if (i == 6)
+			if (i == 6) {
 				--i;
+			}
 			var jj = (dir < 0 ? n - 1 : 0);
 			for (var j = 0; j < n; ++j) {
 				for (var ii = i; ii > i - 2; --ii) {
@@ -332,8 +341,9 @@
 		var n = matrix.length;
 		for (var i = 0; i < n; ++i) {
 			for (var j = 0; j < n; ++j) {
-				if (!reserved[i][j])
+				if (!reserved[i][j]) {
 					matrix[i][j] ^= maskf(i, j);
+				}
 			}
 		}
 		return matrix;
@@ -356,8 +366,9 @@
 		var evaluategroup = function (groups) {
 			var score = 0;
 			for (var i = 0; i < groups.length; ++i) {
-				if (groups[i] >= 5)
+				if (groups[i] >= 5) {
 					score += PENALTY_CONSECUTIVE + (groups[i] - 5);
+				}
 			}
 			for (var i = 5; i < groups.length; i += 2) {
 				var p = groups[i];
@@ -439,25 +450,25 @@
 		return matrix;
 	};
 	var QRCode = {
-		'generate': function (data, options) {
+		"generate": function (data, options) {
 			var MODES = {
-				'numeric': MODE_NUMERIC,
-				'alphanumeric': MODE_ALPHANUMERIC,
-				'octet': MODE_OCTET
+				"numeric": MODE_NUMERIC,
+				"alphanumeric": MODE_ALPHANUMERIC,
+				"octet": MODE_OCTET
 			};
 			var ECCLEVELS = {
-				'L': ECCLEVEL_L,
-				'M': ECCLEVEL_M,
-				'Q': ECCLEVEL_Q,
-				'H': ECCLEVEL_H
+				"L": ECCLEVEL_L,
+				"M": ECCLEVEL_M,
+				"Q": ECCLEVEL_Q,
+				"H": ECCLEVEL_H
 			};
 			options = options || {};
 			var ver = options.version || -1;
-			var ecclevel = ECCLEVELS[(options.ecclevel || 'L').toUpperCase()];
+			var ecclevel = ECCLEVELS[(options.ecclevel || "L").toUpperCase()];
 			var mode = options.mode ? MODES[options.mode.toLowerCase()] : -1;
-			var mask = 'mask' in options ? options.mask : -1;
+			var mask = "mask" in options ? options.mask : -1;
 			if (mode < 0) {
-				if (typeof data === 'string') {
+				if (typeof data === "string") {
 					if (data.match(NUMERIC_REGEXP)) {
 						mode = MODE_NUMERIC;
 					} else if (data.match(ALPHANUMERIC_OUT_REGEXP)) {
@@ -469,85 +480,90 @@
 					mode = MODE_OCTET;
 				}
 			} else if (!(mode == MODE_NUMERIC || mode == MODE_ALPHANUMERIC || mode == MODE_OCTET)) {
-				throw 'invalid or unsupported mode';
+				throw "invalid or unsupported mode";
 			}
 			data = validatedata(mode, data);
-			if (data === null)
-				throw 'invalid data format';
-			if (ecclevel < 0 || ecclevel > 3)
-				throw 'invalid ECC level';
+			if (data === null) {
+				throw "invalid data format";
+			}
+			if (ecclevel < 0 || ecclevel > 3) {
+				throw "invalid ECC level";
+			}
 			if (ver < 0) {
 				for (ver = 1; ver <= 40; ++ver) {
-					if (data.length <= getmaxdatalen(ver, mode, ecclevel))
+					if (data.length <= getmaxdatalen(ver, mode, ecclevel)) {
 						break;
+					}
 				}
-				if (ver > 40)
-					throw 'too large data';
+				if (ver > 40) {
+					throw "too large data";
+				}
 			} else if (ver < 1 || ver > 40) {
-				throw 'invalid version';
+				throw "invalid version";
 			}
-			if (mask != -1 && (mask < 0 || mask > 8))
-				throw 'invalid mask';
+			if (mask != -1 && (mask < 0 || mask > 8)) {
+				throw "invalid mask";
+			}
 			return generate(data, ver, mode, ecclevel, mask);
 		},
-		'generateHTML': function (data, options) {
+		"generateHTML": function (data, options) {
 			options = options || {};
 
 			var fillcolor = options.fillcolor ? options.fillcolor : "#FFFFFF";
 			var textcolor = options.textcolor ? options.textcolor : "#000000";
 
-			var matrix = QRCode['generate'](data, options);
+			var matrix = QRCode["generate"](data, options);
 			var modsize = Math.max(options.modulesize || 5, 0.5);
 			var margin = Math.max(options.margin !== null ? options.margin : 4, 0.0);
-			var e = document.createElement('div');
+			var e = document.createElement("div");
 			var n = matrix.length;
 			var html = ['<table border="0" cellspacing="0" cellpadding="0" style="border:' +
 				modsize * margin + 'px solid ' + fillcolor + ';background:' + fillcolor + '">'];
 			for (var i = 0; i < n; ++i) {
-				html.push('<tr>');
+				html.push("<tr>");
 				for (var j = 0; j < n; ++j) {
 					html.push('<td style="width:' + modsize + 'px;height:' + modsize + 'px' +
 						(matrix[i][j] ? ';background:' + textcolor : '') + '"></td>');
 				}
-				html.push('</tr>');
+				html.push("</tr>");
 			}
-			e.className = 'qrcode';
-			/* e.innerHTML = html.join('') + '</table>'; */
+			e.className = "qrcode";
+			/* e.innerHTML = html.join("") + "</table>"; */
 
 			var range = document.createRange();
 			range.selectNodeContents(e);
-			var frag = range.createContextualFragment(html.join('') + '</table>');
+			var frag = range.createContextualFragment(html.join("") + "</table>");
 			e.appendChild(frag);
 
 			return e;
 		},
-		'generateSVG': function (data, options) {
+		"generateSVG": function (data, options) {
 			options = options || {};
 
 			var fillcolor = options.fillcolor ? options.fillcolor : "#FFFFFF";
 			var textcolor = options.textcolor ? options.textcolor : "#000000";
 
-			var matrix = QRCode['generate'](data, options);
+			var matrix = QRCode["generate"](data, options);
 			var n = matrix.length;
 			var modsize = Math.max(options.modulesize || 5, 0.5);
 			var margin = Math.max(options.margin ? options.margin : 4, 0.0);
 			var size = modsize * (n + 2 * margin);
 			/* var common = ' class= "fg"' + ' width="' + modsize + '" height="' + modsize + '"/>'; */
-			var e = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-			e.setAttribute('viewBox', '0 0 ' + size + ' ' + size);
-			e.setAttribute('style', 'shape-rendering:crispEdges');
+			var e = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+			e.setAttribute("viewBox", "0 0 " + size + " " + size);
+			e.setAttribute("style", "shape-rendering:crispEdges");
 
 			var frag = document.createDocumentFragment();
 
 			/* var svg = ['<style scoped>.bg{fill:' + fillcolor + '}.fg{fill:' + textcolor + '}</style>', '<rect class="bg" x="0" y="0"', 'width="' + size + '" height="' + size + '"/>', ]; */
-			var style = document.createElementNS('http://www.w3.org/2000/svg', 'style');
-			style.appendChild(document.createTextNode('.bg{fill:' + fillcolor + '}.fg{fill:' + textcolor + '}'));
+			var style = document.createElementNS("http://www.w3.org/2000/svg", "style");
+			style.appendChild(document.createTextNode(".bg{fill:" + fillcolor + "}.fg{fill:" + textcolor + "}"));
 			style.setAttribute("scoped", "scoped");
 
 			frag.appendChild(style);
 
 			var createRect = function (c, f, x, y, s) {
-				var fg = document.createElementNS('http://www.w3.org/2000/svg', 'rect') || "";
+				var fg = document.createElementNS("http://www.w3.org/2000/svg", "rect") || "";
 				fg.setAttributeNS(null, "class", c);
 				fg.setAttributeNS(null, "fill", f);
 				fg.setAttributeNS(null, "x", x);
@@ -573,28 +589,28 @@
 				yo += modsize;
 			}
 
-			/* e.innerHTML = svg.join(''); */
+			/* e.innerHTML = svg.join(""); */
 			e.appendChild(frag);
 
 			return e;
 		},
-		'generatePNG': function (data, options) {
+		"generatePNG": function (data, options) {
 			options = options || {};
 
 			var fillcolor = options.fillcolor ? options.fillcolor : "#FFFFFF";
 			var textcolor = options.textcolor ? options.textcolor : "#000000";
 
-			var matrix = QRCode['generate'](data, options);
+			var matrix = QRCode["generate"](data, options);
 			var modsize = Math.max(options.modulesize || 5, 0.5);
 			var margin = Math.max(options.margin != null ? options.margin : 4, 0.0);
 			var n = matrix.length;
 			var size = modsize * (n + 2 * margin);
-			var canvas = document.createElement('canvas'),
+			var canvas = document.createElement("canvas"),
 			context;
 			canvas.width = canvas.height = size;
-			context = canvas.getContext('2d');
+			context = canvas.getContext("2d");
 			if (!context)
-				throw 'canvas support is needed for PNG output';
+				throw "canvas support is needed for PNG output";
 			context.fillStyle = fillcolor;
 			context.fillRect(0, 0, size, size);
 			context.fillStyle = textcolor;
